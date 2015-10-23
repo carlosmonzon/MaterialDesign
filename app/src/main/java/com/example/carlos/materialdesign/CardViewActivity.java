ea7f9796d552c57ecf053077ef11e74166ebbf86
Carlos Monzon <carlosmonzonsalvador@gmail.com>
2015-10-23 18:03:02 -0500

    CardViewActivty

--- app/src/main/java/com/example/carlos/materialdesign/CardViewActivity.java
+++ app/src/main/java/com/example/carlos/materialdesign/CardViewActivity.java
@@ -1,33 +1,23 @@
 package com.example.carlos.materialdesign;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CardViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.item_card);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_card_view_activty, menu);
         return true;
-    }
-
-    public void nextScreen(View view) {
-        startActivity(new Intent(this, ToolbarActivity.class));
-    }
-
-    public void goToCardViewActivity(View view) {
-        startActivity(new Intent(this, CardViewActivity.class));
     }
 
     @Override
